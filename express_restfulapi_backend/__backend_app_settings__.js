49c223bec34151333e6a77aee553677e0220b75f
Reza Naeemi <rezauw@uw.edu>
2018-04-08 22:03:32 -0700

    Fixed. Need to check performance on Mac

--- express_restfulapi_backend/__backend_app_settings__.js
+++ express_restfulapi_backend/__backend_app_settings__.js
@@ -5,4 +5,4 @@ module.exports.LISTEN_ON_PORT_NUMBER = 3001;
 module.exports.CACHE_TTL = 86400;  // 86400 seconds = 24 hours
 
 
-module.exports.USE_FAKE_DATA = false;
+module.exports.USE_FAKE_DATA = true;
--- react-redux_frontend/src/components/content/ExistingInstancesTable.js
+++ react-redux_frontend/src/components/content/ExistingInstancesTable.js
@@ -1,55 +1,114 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
 import CircularProgress from 'material-ui/CircularProgress';
-
 
 import 'bootstrap/dist/css/bootstrap.css';
 import 'react-bootstrap-table/dist/react-bootstrap-table-all.min.css';
 
 
-var fakeExistingInstancesTableHeaderColumns = [{
-  datatype: "string",
-  option: "BISTName",
-  placeholder: "NULL"
-}, {
-  compiler: "171",
-  datatype: "integer",
-  option: "ID",
-  placeholder: "171"
-}];
+export default class ExistingInstancesTable extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state= {
+      products: []
+    }
 
 
-var fakeExistingInstancesTableData = [{
-  compilerValue: 0,
-  compilerName: "Compiler0",
-}, {
-  compilerValue: 1,
-  compilerName: "Compiler1",
-}, {
-  compilerValue: 2,
-  compilerName: "Compiler2",
-}];
+    this.addProducts(5000);
+  }
 
 
+  addProducts(quantity) {
 
-const cellEditProp = {
-  mode: 'click'
-};
-
-const ExistingInstancesTable = (props) => (
-
+    const startId = this.state.products.length;
+    for (let i = 0; i < quantity; i++) {
+      const id = startId + i;
+  
+      this.state.products.push({
+        id: id,
+        name: 'Item name ' + id,
+        price: 2100 + i
+      });
+    }
+  
+  }
   
 
-  <BootstrapTable data={fakeExistingInstancesTableData} keyField='compilerValue' cellEdit={ cellEditProp }>
-    <TableHeaderColumn dataField={'compilerValue'} editable={false}>Compiler Value</TableHeaderColumn>
-    <TableHeaderColumn dataField={'compilerName'} editable={false}>Compiler Name</TableHeaderColumn>
-    <TableHeaderColumn dataField={'instanceName'}>Instance Name</TableHeaderColumn>
-    {fakeExistingInstancesTableHeaderColumns.map((col, index) => {
-      return (
-        <TableHeaderColumn key={index} dataField={'option' + index}>{col.option}</TableHeaderColumn>
-      );
-    })}
-  </BootstrapTable>
-)
 
-export default ExistingInstancesTable;
+
+  onAfterSaveCell(row, cellName, cellValue) {
+
+    alert(`Save cell ${cellName} with value ${cellValue}`);
+  
+    let rowStr = '';
+    for (const prop in row) {
+      rowStr += prop + ': ' + row[prop] + '\n';
+    }
+  
+    
+    alert('The whole row :\n' + rowStr);
+  
+  }
+  
+
+
+
+  onBeforeSaveCell(row, cellName, cellValue) {
+    /* 
+      You can do any validation on here for editing value,
+      return false for reject the editing
+    */
+  
+    return true;
+  }  
+
+
+  onBeforeSaveCellAsync(row, cellName, cellValue, done) {
+    /*
+      If you validation is async, for example: you want to pop a confirm dialog
+      for user to confirm in this case, react-bootstrap-table passes a callback 
+      function back to you.
+  
+      You are suppose to call this callback function with a bool value to perform
+      if it is valid or not. In addition, you sould return 1 to tell react-bootstrap-table
+      this is a async operation.
+    */
+  
+    /*
+    // I use setTimeout to perform a async operation.
+    setTimeout(() => {
+  
+      done(true);   // it's ok to save :)
+      done(false);  // it's not ok to save :(
+  
+    }, 3000);
+  
+    
+    return 1;   // please return 1
+    */
+  
+  }
+
+
+
+  cellEditProp = {
+    mode: 'click',
+    blueToSave: true,
+    beforeSaveCell: this.onBeforeSaveCell,   // a hook for before saving cell
+    afterSaveCell: this.onAfterSaveCell,     // a hook for after saving cell
+  }
+
+
+  render() {
+    return (
+      <BootstrapTable data={ this.state.products } height={350} cellEdit={ this.cellEditProp }>
+        <TableHeaderColumn dataField='id' isKey={ true }>Product ID</TableHeaderColumn>
+        <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
+        <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
+      </BootstrapTable>
+    );
+  }
+
+}
